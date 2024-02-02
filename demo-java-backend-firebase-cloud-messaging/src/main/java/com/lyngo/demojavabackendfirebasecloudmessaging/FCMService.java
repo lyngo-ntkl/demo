6144fbab9afac015e05a58ffb44176e994f1a2e1
lyngo-ntkl <lyngo.ntkl@gmail.com>
2024-02-02 14:27:19 +0700

    initialize demo firebase cloud messaging project

---
+++ demo-java-backend-firebase-cloud-messaging/src/main/java/com/lyngo/demojavabackendfirebasecloudmessaging/FCMService.java
@@ -0,0 +1,17 @@
+package com.lyngo.demojavabackendfirebasecloudmessaging;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.*;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class FCMService {
+
+}
